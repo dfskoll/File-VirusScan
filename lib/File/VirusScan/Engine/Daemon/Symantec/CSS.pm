4ff19b8221c9c99acff1341d77be8a5040e2cdd2
Dianne Skoll <dianne@skoll.ca>
2022-05-24 12:31:04 -0400

    Allow scanning of relative pathnames (we convert to absolute first).

--- lib/File/VirusScan/Engine/Daemon/Symantec/CSS.pm
+++ lib/File/VirusScan/Engine/Daemon/Symantec/CSS.pm
@@ -61,9 +61,10 @@ sub scan
 {
 	my ($self, $path) = @_;
 
-	if(abs_path($path) ne $path) {
-		return File::VirusScan::Result->error("Path $path is not absolute");
-	}
+        my $abs = abs_path($path);
+        if ($abs && $abs ne $path) {
+                $path = $abs;
+        }
 
 	my @files = eval { $self->list_files($path) };
 	if($@) {
